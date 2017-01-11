412c3661a16cb423f03f90274984fd849d616b89
Reethi Joseph <reethi94@hotmail.com>
2017-01-11 17:43:22 +0530

    Update ReverseWord.java

--- ReverseWord.java
+++ ReverseWord.java
@@ -1,19 +1,26 @@
-public class Solution {
-	public String reverseWords(String inputString) {
-	    String[] words = inputString.split(" ");
+import java.util.*;
+import java.io.*;
+public class ReverseWords {
+    public static String reverseWords(String inputString) {
+         String[] words = inputString.split(" ");
          
-        StringBuffer reverseString =new StringBuffer();// "";
-        if(words.length==1) return inputString.trim();
-         int n=words.length,i;
-        for(i=n - 1; i >= 0; i--)
-        {
-
-     if(words[i] != null && !words[i].isEmpty())
-
-            reverseString=reverseString.append(words[i]).append(" ");
-        }
-        String s= reverseString.toString();
-       s=s.trim();
-        return s;
-	}
+            StringBuffer reverseString =new StringBuffer();// "";
+            if(words.length==1) 
+            return inputString.trim();
+            int n=words.length,i;
+            for(i=n - 1; i >= 0; i--){
+                if(words[i] != null && !words[i].isEmpty())
+                    reverseString=reverseString.append(words[i]).append(" ");
+             }
+            String s= reverseString.toString();
+            s=s.trim();
+            return s;
+    }
+    public static void Main(String args[]){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Please enter a string");
+        String s=in.nextLine();
+        String rev=reverseWords(s);
+        System.out.println(rev);
+    }
 }
